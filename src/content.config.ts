23d20b946579728b17b024339f1028d0dc1dd026
AadityaNair <AadityaNair@users.noreply.github.com>
2026-02-18 23:41:20 +0000

    Support .md extension for blog posts

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 const blogs = defineCollection({
-  loader: glob({ pattern: "**/*.mdx", base: "./src/blogs" }),
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/blogs" }),
   schema: z.object({
     title: z.string(),
     excerpt: z.string(),
