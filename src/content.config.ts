3e1027968195967aa5b08645c41e710a325e28a2
AadityaNair <AadityaNair@users.noreply.github.com>
2026-02-18 23:29:36 +0000

    Create a blog and link it to the front page

---
+++ src/content.config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+
+const blogs = defineCollection({
+  loader: glob({ pattern: "**/*.mdx", base: "./src/blogs" }),
+  schema: z.object({
+    title: z.string(),
+    excerpt: z.string(),
+    date: z.coerce.date(),
+    tags: z.array(z.string()).default([]),
+  }),
+});
+
+export const collections = { blogs };
