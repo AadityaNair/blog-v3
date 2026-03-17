c8496958c654be125b10da92be2a2d49f8e6f849
AadityaNair <AadityaNair@users.noreply.github.com>
2026-03-17 21:02:10 +0000

    Add social links

--- src/consts.ts
+++ src/consts.ts
@@ -1,1 +1,8 @@
 export const POSTS_PER_PAGE = 5;
+
+export const SOCIAL_LINKS = {
+  linkedin: "https://linkedin.com/in/aadityanair",
+  github: "https://github.com/AadityaNair",
+  email: "mailto:me@amnair.dev",
+  resume: "/resume.pdf",
+};
